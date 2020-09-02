3dee86103e41a0feed01d07675584cac797b6d12
AndresFelipe11 <andres.1701714898@gmail.com>
2020-09-02 18:01:05 -0500

    Clase empleadoAsalariado

--- src/modelos/EmpleadoAsalariado.java
+++ src/modelos/EmpleadoAsalariado.java
@@ -1,5 +1,17 @@
 package modelos;
 
-public class EmpleadoAsalariado {
+public class EmpleadoAsalariado extends Empleado {
+	private double salarioSemanal;
+	
+	public EmpleadoAsalariado(String identificador, String nombre) {
+		super(identificador, nombre);
+		
+	}
+	
+	@Override
+	public double calcularSalario() {
+		
+		return this.salarioSemanal;
+	}
 
 }
