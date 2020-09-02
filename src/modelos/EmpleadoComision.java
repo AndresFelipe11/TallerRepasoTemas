b971e137cf55aa50b6b01cd8e16a2f300590bdfa
Camilog699 <Amraga10@gmail.com>
2020-09-02 13:08:26 -0500

    Logica de clases de empleados

--- src/modelos/EmpleadoComision.java
+++ src/modelos/EmpleadoComision.java
@@ -1,5 +1,18 @@
 package modelos;
 
-public class EmpleadoComision {
+public class EmpleadoComision extends Empleado{
+	private double salarioSemanal;
+	private double valorVentas;
+
+	public EmpleadoComision(String identificador, String nombre, double salarioSemanal, double valorVentas) {
+		super(identificador, nombre);
+		this.salarioSemanal = salarioSemanal;
+		this.valorVentas = valorVentas;
+	}
+
+	@Override
+	public double calcularSalario() {
+		return salarioSemanal + (valorVentas * 0.05);
+	}
 
 }
--- src/modelos/EmpleadoHora.java
+++ src/modelos/EmpleadoHora.java
@@ -1,5 +1,21 @@
 package modelos;
 
-public class EmpleadoHora {
-
+public class EmpleadoHora extends Empleado{
+	private double valorHora;
+	private int horasTrabajadas;
+	
+	public EmpleadoHora(String identificador, String nombre, double valorHora, int horasTrabajadas) {
+		super(identificador, nombre);
+		this.valorHora = valorHora;
+		this.horasTrabajadas = horasTrabajadas;
+	}
+	
+	
+	@Override
+	public double calcularSalario() {
+		if (horasTrabajadas > 40) {
+			return valorHora*horasTrabajadas + 200000;
+		}
+		return valorHora*horasTrabajadas; 
+	}
 }
