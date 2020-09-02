849ed7ca0c4dbf9360daee930042af5a38130b35
AndresFelipe11 <andres.1701714898@gmail.com>
2020-09-02 17:56:26 -0500

    creacin metodo abstracto calcularSalario

--- src/modelos/Empleado.java
+++ src/modelos/Empleado.java
@@ -1,5 +1,18 @@
 package modelos;
 
 public abstract class Empleado {
+	
+	
+	private String identificador;
+	private String nombre;
+	
+	
+	public Empleado(String identificador, String nombre) {
+		
+		this.identificador = identificador;
+		this.nombre = nombre;
+	}
 
+	public abstract double calcularSalario();
+	
 }
